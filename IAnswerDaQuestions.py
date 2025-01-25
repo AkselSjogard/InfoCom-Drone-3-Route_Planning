9bcc7f68765a7c81507648ff29ec3f5d7aa65ce3
AkselSjogard <aksel.sjogard@telia.com>
2025-01-25 22:03:50 +0100

    I GROW MORE POWERFUL

---
+++ IAnswerDaQuestions.py
@@ -0,0 +1,5 @@
+the url of route planner: http://127.0.0.1:5002/planner
+
+the web client sends the adresses in text form to the route planner, the route planner reads the current location from the redis database,
+and the database sends all of the coordinates into the drone controller
+
